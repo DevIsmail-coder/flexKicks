f7ab7353415cf50f40eda6815f558e58231367e9
DevIsmail-coder <ismailolawale777@gmail.com>
2025-03-07 23:44:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import './App.css'
+import Home from './pages/home/home'
+import Menpage from './pages/menpage/Menpage'
+import Women from './pages/women/Women'
+import Baby from './pages/baby/Baby'
+import Details from './pages/details/Details'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Checkout from './pages/checkout/Checkout'
+import Cart from './pages/cart/Cart'
+import Contact from './pages/contact/Contact'
+
+
+function App() {
+  
+    const routes = createBrowserRouter([
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/menpage",
+        element: <Menpage />
+      },
+      {
+        path: "/womenpage",
+        element: <Women />
+      },
+      {
+        path: "/babypage",
+        element: <Baby/>
+      },
+      {
+        path: "/detailspage",
+        element: <Details />
+      },
+      {
+        path: "/loginpage",
+        element: <Menpage />
+      },
+      {
+        path: "/loginpage",
+        element: <Login />
+      },
+      {
+        path: "/signuppage",
+        element: <Signup />
+      },
+      {
+        path: "/chechout",
+        element: <Checkout />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ])
+  return <div className='Appbody'>
+    <RouterProvider router={routes} />
+  </div>
+}
+
+export default App
---
+++ src/pages/menpage/Menpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menpage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menpage
