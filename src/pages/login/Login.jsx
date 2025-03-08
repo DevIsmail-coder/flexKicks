2e835a98ef76f18a51befff0e5ecbc94c43536db
Bigjosh751 <you@example.com>
2025-03-08 23:14:51 +0100

    updated the login_page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="login-container">
+            <div className="login-box">
+                <h2>Login</h2>
+                <p>Enter Login details to get access</p>
+                <label htmlFor="username"> Username Or Email Address</label>
+                <input 
+                    type="text" 
+                    placeholder="Username / Email address" 
+                    className="login-input" 
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password" 
+                    placeholder="Enter Password" 
+                    className="login-input" 
+                />
+                <div className="login-options">
+                    <label>
+                        <input type="checkbox"  /> Keep Me Logged In
+                    </label>
+                    <a href="#" className="forgot-password">Forgot Password?</a>
+                </div>
+               <div className="login-btn">
+               <p>
+                    Don’t have an account? <a href="#" className="signup-link">Sign Up</a> here
+                </p>
+               <button className="login-button">Login</button>
+               
+               </div>
+               
+            </div>
+        </div>
   )
 }
 
