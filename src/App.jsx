5fa87161df732b6747c806c069d7ddb0cc6531e8
DevIsmail-coder <ismailolawale777@gmail.com>
2025-03-10 08:16:02 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Checkout from './pages/checkout/Checkout'
 import Cart from './pages/cart/Cart'
 import Contact from './pages/contact/Contact'
 import Header from './pages/header/header'
+import Home from './pages/home/home'
 
 
 function App() {
